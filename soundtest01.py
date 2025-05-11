08658a429b69f99fc8007999a9befec2e2b7a319
qngt <wyj802@haw-hamburg.de>
2025-05-11 21:41:30 +0200

    test

--- soundtest01.py
+++ soundtest01.py
@@ -1,14 +1,18 @@
 import numpy as np
 import sounddevice as sd
 
+
 fs = 44100  # Abtastrate (Hz)
 duration = 5.0  # Dauer in Sekunden
 frequency = 100  # Frequenz in Hz
 
 t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    #linspace intervall 0 bis 5, anzahl der werte die erzeugt werden, Endwert (5.0) nicht eingeschlossen
-tone = 0.5 * np.sin(2 * np.pi * frequency * t) #ZeitArray: tone
+#linspace intervall 0 bis 5, anzahl der werte die erzeugt werden, Endwert (5.0) nicht eingeschlossen
+
+tone = 0.5 * np.sin(2 * np.pi * frequency * t)
+#ZeitArray: tone : 0.5 Faktor für Lautstärkeregelung sonst Übersteuerung
+#
 
 sd.play(tone, samplerate=fs)
-sd.wait()  # Warten, bis Ton fertig ist
+sd.wait()  #Warten, bis Ton fertig ist
 
