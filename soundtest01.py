c30d620a83b0740a8bf972d6332323a877881d92
quang <wyj802@haw-hamburg.de>
2025-05-12 11:03:40 +0200

    1205

--- soundtest01.py
+++ soundtest01.py
@@ -2,12 +2,12 @@ import numpy as np
 import sounddevice as sd
 
 
-fs = 44100  # Abtastrate (Hz)
+fs = 44100  # Abtastrate (Hz) -> Abtasttheorem
 duration = 5.0  # Dauer in Sekunden
 frequency = 100  # Frequenz in Hz
 
 t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-#linspace intervall 0 bis 5, anzahl der werte die erzeugt werden, Endwert (5.0) nicht eingeschlossen
+#linspace intervall 0 bis 5, anzahl der x-werte die erzeugt werden,(duration)-Wert nicht eingeschlossen
 
 tone = 0.5 * np.sin(2 * np.pi * frequency * t)
 #ZeitArray: tone : 0.5 Faktor für Lautstärkeregelung sonst Übersteuerung
@@ -16,3 +16,5 @@ tone = 0.5 * np.sin(2 * np.pi * frequency * t)
 sd.play(tone, samplerate=fs)
 sd.wait()  #Warten, bis Ton fertig ist
 
+#Wie möchte ich mehrere Töne hintereinander abspielen lassen? eig ganz gut manuell weil ich nur frquency var
+#umschreiben muss. Werte
